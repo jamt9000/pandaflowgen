5b146314daa4dadc9574b858db81bb76f6be7710
James Thewlis <jdt@robots.ox.ac.uk>
2017-07-06 15:11:01 +0100

    Rotating cube orthographic projection

---
+++ cubeseqs.sh
@@ -0,0 +1,11 @@
+mkdir out
+python cubeortho.py 1
+mv out cube1; mkdir out
+python cubeortho.py 1.5
+mv out cube2; mkdir out
+python cubeortho.py 0.5
+mv out cube3; mkdir out
+python cubeortho.py -0.5
+mv out cube4; mkdir out
+python cubeortho.py -1
+mv out cube5; mkdir out
